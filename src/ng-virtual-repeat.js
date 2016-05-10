52f4258a6eca26f670bcc479f51e0fb4da101ad3
Klajd <klajd.diko@gmail.com>
2016-05-10 23:34:05 +0200

    new rendering implementation

--- src/ng-virtual-repeat.js
+++ src/ng-virtual-repeat.js
@@ -1,225 +1,127 @@
-angular
-    .module('ngVirtualRepeat')
-    .directive('ngVirtualRepeat', NgVirtualRepeatDirective);
+(function () {
+    "use strict";
 
-NgVirtualRepeatDirective.$inject = ['$log', '$rootElement'];
-function NgVirtualRepeatDirective($log, $rootElement) {
-    return {
-        restrict: 'A',
-        link: link,
-        scope: {
-            data: '=ngVirtualRepeat'
-        }
-    };
+    angular
+        .module('ngVirtualRepeat')
+        .directive('ngVirtualRepeat', NgVirtualRepeatDirective);
 
-    function link(scope, element, attrs) {
-        var viewport = findParent(element, 'ng-virtual-repeat-container');
-        var container = viewport.find('div').eq(0);
-        var renderOffset = 20;  //how element to render out of the viewport
-        var rowHeight = 37;
-        var rowWidth = 0;
-        var scrollTop = 0;
-        var scrollLeft = 0;
-        var rendered = [];
-        var data = scope.data;
-        var matrix = {
-            data: data,
-            rows: {
-                // rendered: undefined,
-                total: data.length,
-                active: 0,
-                visible: 0,
-                firstActive: 0,
-                first: 0
-            },
-            cols: {
-                // rendered: undefined,
-                total: data.length > 0 ? data[0].length : 0,
-                active: 0,
-                visible: 0,
-                firstActive: 0,
-                first: 0
+    NgVirtualRepeatDirective.$inject = [];
+    function NgVirtualRepeatDirective() {
+        return {
+            restrict: 'A',
+            link: link,
+            scope: {
+                matrix: '=ngVirtualRepeat',
+                cellHeight: '@',
+                cellWidth: '@'
             }
         };
 
-        scope.$watch(function () { return matrix; }, render, true);
+        function link(scope, element, attrs) {
+            var viewport = findParent(element, 'ng-virtual-repeat-container');
+            var container = viewport.find('div').eq(0);
 
-        // row height calculate
-        // var td = element.find('td')[0];
-        // rowHeight = td.clientHeight;
-        // console.log(td.clientHeight, td.clientWidth);
-        container.css({ 'padding-top': matrix.rows.first * rowHeight + 'px' });
-        container.css({ 'height': matrix.rows.total * rowHeight + 'px' });
-        var i = 0;
-        viewport.bind('scroll', function onViewportScroll(event) {
-            var isHorizontal = scrollTop === event.target.scrollTop;
-            var isVertical = scrollLeft === event.target.scrollLeft;
+            scope.cellHeight = parseInt(scope.cellHeight) || 40;
+            scope.cellWidth = parseInt(scope.cellWidth) || 70;
+            // TODO make possible to watch in case of viewport resize
+            var visibleRows = Math.ceil(viewport[0].clientHeight / scope.cellHeight);
+            var visibleCols = Math.ceil(viewport[0].clientWidth / scope.cellWidth);
 
-            scrollTop = event.target.scrollTop;
-            scrollLeft = event.target.scrollLeft;
-
-            // cols
-            // TODO
-
-            // if (isVertical && needLoad) {
-
-            // }
-
-            // rows
-            var m = {
-                data: matrix.data
-            };
-
-            m.rows = {
-                first: Math.floor(scrollTop / rowHeight),
-                firstActive: 0, // CALCULATE
-                active: 0,
-                visible: Math.ceil(viewport[0].clientHeight / rowHeight),
-                total: matrix.rows.total
-            };
-
-            m.cols = {}
-
-            // if (isHorizontal && needLoadHorizontal) {
-
-            // }
-
-            console.log(m.rows.first)
-
-            render(m, matrix);
-            matrix.rows = m.rows;
-        });
-
-        function render(matrix, oldMatrix) {
-            if (!matrix) return;
-
-            // source change
-            // if (matrix.data != oldMatrix.data) {
-            //   element.empty();
-            // }
-
-            // is first render for matrix
-            if (rendered.length === 0) {
-                var start = 0;
-                var end = 20;
-                var startCol = 0;
-                var endCol = 50;
-
-                for (var r = start; r < end; r++) {
-                    var trElement = angular.element('<tr>');
-                    for (var c = startCol; c < endCol; c++) {
-                        trElement.append(['<td>', matrix.data[r][c], '</td>'].join(''));
-                    }
-                    rendered.push(trElement);
-                    element.append(trElement);
+            scope.$watch('matrix', function (newValue, oldValue) {
+                // visibleRows = Math.ceil(viewport[0].clientHeight / scope.cellHeight);
+                // visibleCols = Math.ceil(viewport[0].clientWidth / scope.cellWidth);
+                if (newValue === undefined) {
+                    element.empty();
+                    element.append('<tr><td></td><tr>');
+                } else {
+                    render(scope.matrix, [0, 0]);
                 }
-                return;
-            }
-            var movedVertical = matrix.rows.first - oldMatrix.rows.first;
-            var movedHorizontal = matrix.cols.first - oldMatrix.cols.first;
-
-            if (movedVertical) {
-                var bottom = movedVertical > 0;
-                var delta = Math.abs(movedVertical);
-                var removeAction = bottom ? 'shift' : 'pop';
-                var appendAction = bottom ? 'push' : 'unshift';
-                var domAction = bottom ? 'append' : 'prepend';
-
-                delta = delta > matrix.rows.total ? matrix.rows.total : delta;
-                delta = delta > 20 ? 20 : delta;
-
-                // buffer teknik
-                for (var i = 0; i < delta; i++) {
-                    var removed = rendered[removeAction]();
-                    removed.remove();
-
-                    var rowIndex = matrix.rows.first + matrix.rows.visible + i;
-                    //console.debug(matrix.rows.first, rowIndex);
-                    var appended = getRow(rowIndex, 0, 50);
-                    element[domAction](appended);
-                    rendered[appendAction](appended);
-                    // console.debug(rowIndex, appended);
-                }
-
-            }
-
-            container.css({ 'padding-top': matrix.rows.first * rowHeight + 'px' });
-            container.css({ 'height': matrix.rows.total * rowHeight + 'px' });
-
-            //viewport[0].scrollTop = viewport[0].clientHeight + viewport[0].scrollHeight;
-
-            if (movedHorizontal) {
-                // var right = movedHorizontal > 0;
-                // TODO
-
-            }
-
-            function getRow(rowIndex, startCol, endCol) {
-                var trElement = angular.element('<tr>');
-                for (var c = startCol; c < endCol; c++) {
-                    trElement.append(['<td>', matrix.data[rowIndex][c], '</td>'].join(''));
-                }
-                return trElement;
-            }
-
-        }
-
-        //var viewportStyle = window.getComputedStyle(td);
-        // var rH = computeRowHeight(td);
-    }
-
-    function appendRow(row, rowIndex) {
-        var tr = "<tr>";
-        angular.forEach(row, function (cell, index) {
-
-            //TODO remove
-            if (index === 0)
-                tr += ['<td class="row-count">', rowIndex, '</td>'].join('');
-
-            if (index > 50)
-                return;
-
-            tr += ['<td>', cell, '</td>'].join('');
-        });
-
-        tr += '</tr>';
-        return tr;
-    }
-
-
-    /**
-     * Find parent element by selector.
-     * @param {jqElement} element - current element.
-     * @param {string} selector - class selector.
-     * @returns {jqElement} - the parent element found or undefined if none.
-     */
-    function findParent(element, selector) {
-        if (!element) return;
-        if (!selector)
-            //todo refactor error
-            throw new Error('findParent, selector not specified');
-
-        var parent = element.parent();
-        while (parent) {
-
-            if (parent.hasClass(selector))
-                return parent;
-
-            parent = parent.parent();
-        }
-        return parent;
-    }
-
-    /**
-     * Clip the range.
-     */
-    function clip(value, min, max) {
-        if (angular.isArray(value)) {
-            return angular.forEach(value, function (v) {
-                return clip(v, min, max);
             });
-        }
-        return Math.max(min, Math.min(value, max));
-    }
 
-}
+            viewport.bind('scroll', function onViewportScroll(event) {
+                var startCell = [
+                    Math.floor(event.target.scrollTop / scope.cellHeight),
+                    Math.floor(event.target.scrollLeft / scope.cellWidth),
+                ];
+
+                render(scope.matrix, startCell);
+            });
+
+            function render(matrix, startCell) {
+                if (!matrix) return;
+
+                element.empty();
+
+                var startRow = startCell[0]
+                var StartCol = startCell[1]
+                var endRow = startCell[0] + visibleRows + 1;
+                var endCol = startCell[1] + visibleCols + 1;
+                var totalRows = matrix.length;
+                var totalCols = matrix[0].length;
+                if (endRow > totalRows) endRow = totalRows;
+                if (endCol > totalCols) endCol = totalCols;
+
+                element.append(generateTemplate(matrix, startCell, [endRow, endCol]));
+
+                container.css({ 'padding-top': startRow * scope.cellHeight + 'px' });
+                container.css({ 'height': (totalRows - visibleRows) * scope.cellHeight + 'px' });
+                container.css({ 'padding-left': StartCol * scope.cellWidth + 'px' });
+                container.css({ 'width': (totalCols - visibleCols) * scope.cellWidth + 'px' });
+            }
+        }
+
+        /**
+         * Generate table html template from matrix and the diplay port
+         * @param {Object} matrix - the matrix handling data.
+         * @param {Array} start - the top left koordinate of matrix where begin display
+         * @param {Array} end - the bottom right koordinate of the matrix where ends display
+         * @returns {String} - the html generated tempalte.
+         */
+        function generateTemplate(matrix, start, end) {
+            var template = '';
+            for (var r = start[0]; r < end[0]; r++) {
+                var trElement = '<tr>';
+                for (var c = start[1]; c < end[1]; c++) {
+                    trElement += ['<td>', matrix[r][c], '</td>'].join('');
+                }
+                template += trElement;
+            }
+            return template;
+        }
+        
+        // TODO remove
+        function generateJqTemplate(){
+            // for (var r = start; r < end; r++) {
+            //     var trElement = angular.element('<tr>');
+            //     for (var c = startCol; c < endCol; c++) {
+            //         trElement.append(['<td>', matrix.data[r][c], '</td>'].join(''));
+            //     }
+            //     element.append(trElement);
+            // }
+        }
+
+        /**
+         * Find parent element by selector.
+         * @param {jqElement} element - current element.
+         * @param {string} selector - class selector.
+         * @returns {jqElement} - the parent element found or undefined if none.
+         */
+        function findParent(element, selector) {
+            if (!element) return;
+            if (!selector)
+                //todo refactor error
+                throw new Error('findParent, selector not specified');
+
+            var parent = element.parent();
+            while (parent) {
+
+                if (parent.hasClass(selector))
+                    return parent;
+
+                parent = parent.parent();
+            }
+            return;
+        }
+
+    }
+})();
