1026ddac3414a4a7616943cdbb8e3088fa22ec4c
Klajd <klajd.diko@gmail.com>
2016-05-12 09:26:01 +0200

    add gulp publishing

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
+var args = require('yargs').argv;
 var del = require('del');
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var gif = require('gulp-if');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var stripDebug = require('gulp-strip-debug');
 var config = {
     dest: './dist/',
     src: './src/',
@@ -17,9 +20,12 @@ gulp.task('build', ['clean'], function () {
     var files = []
         .concat(config.files)
         .map(function (file) { return config.src + file; });
+        
+    var isPublishing = args.publish || args.pub || args.p;
 
     return gulp
         .src(files)
+        .pipe(gif(isPublishing, stripDebug()))
         .pipe(concat(config.jsExport))
         .pipe(gulp.dest(config.dest))
         .pipe(uglify())
